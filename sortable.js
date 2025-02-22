31f364530ac37c04e4b14abdb94c3dae0528d195
amin <aungkhantmin.dev@gmail.com>
2025-02-22 13:21:08 +0200

    error added

--- sortable.js
+++ sortable.js
@@ -269,16 +269,41 @@ function heroes(heroes) {
     searchbar.type = 'text'
     document.body.appendChild(searchbar)
 
-    searchbar.addEventListener('input', (event) => {
-        const searchTerm = event.target.value;
+    //error variables
+    let showedOnce = false;
+    let errorMessage = null;
 
-        heroesFiltered = heroes.filter((hero) => hero.name.toLowerCase().includes(searchTerm.toLowerCase()))
-        sortByColumn(heroesFiltered, false, sortBy)
+searchbar.addEventListener('input', (event) => {
+    const searchTerm = event.target.value;
+   
+    heroesFiltered = heroes.filter((hero) => 
+        hero.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-        tBody.remove()
-        tBody = makeTableBody(heroesFiltered)
+    //if not found --> remove head, body, show error. Else ---> add them back
+    if (heroesFiltered.length === 0 && !showedOnce) {
+        tHead.remove();
+        tBody.remove();
+        errorMessage = document.createElement('div');
+        errorMessage.innerHTML = "Sorry, the hero you were searching for does not exist!";
+        errorMessage.style.color = "red";
+        document.body.appendChild(errorMessage);
+        showedOnce = true;
+    } else if (heroesFiltered.length > 0) {
+        if (errorMessage) {
+            errorMessage.remove(); 
+            errorMessage = null;
+        }
+        showedOnce = false;
+
+        sortByColumn(heroesFiltered, false, sortBy);
+
+        tBody.remove();
+        tBody = makeTableBody(heroesFiltered);
+        table.appendChild(tHead);
         table.appendChild(tBody);
-    });
+    }
+});
 
     // make the table
     const table = document.createElement('table')
